81323dc2b27201ddd1fc7d63239a3eab4cf85454
jaaaain <1079505600@qq.com>
2025-11-14 20:11:09 +0800

    新建项目

---
+++ src/main/java/com/jaaaain/jresonance/JResonanceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jaaaain.jresonance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JResonanceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JResonanceBackendApplication.class, args);
+	}
+
+}
