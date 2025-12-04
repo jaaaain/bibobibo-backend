a57ca422bf53d88e1e3348cc60e7289f0043b62a
jaaaain <1079505600@qq.com>
2025-12-04 23:51:54 +0800

    OSS基础配置

---
+++ src/main/java/com/jaaaain/jresonance/common/config/OSSConfig.java
@@ -0,0 +1,31 @@
+package com.jaaaain.jresonance.common.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.Serializable;
+
+@Data
+@Configuration
+public class OSSConfig implements Serializable {
+
+    @Value("${oss.bucket}")
+    private String bucket;
+
+    @Value("${oss.endpoint}")
+    private String endpoint;
+
+    @Value("${oss.url}")
+    private String url;
+
+    @Value("${oss.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${oss.accessKeySecret}")
+    private String accessKeySecret;
+
+    @Value("${oss.idleTimeout}")
+    private long idleTimeout;
+ 
+}
